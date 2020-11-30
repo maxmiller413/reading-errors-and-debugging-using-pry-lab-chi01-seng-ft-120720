c8bf6e4967fdb7b6af946d9c9a0d9e648f92ddd3
Max Miller <maxmiller413@gmail.com>
2020-11-30 04:33:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,5 +1,5 @@
-def snake_it_up(string {#})
-  if string[#] = "s"
+def snake_it_up(string)
+  if string[0] = "s"
     ten_times = (10 * "s".to_i) + "string".to_i 
     binding.pry 
     puts ten_times
